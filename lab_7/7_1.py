e367f2e7e9fe22cd2f572372617ad2f7bab4f112
Artemusss <142901408+Artemusss@users.noreply.github.com>
2024-05-15 14:12:04 +0300

    Update 7_1.py

--- lab_7/7_1.py
+++ lab_7/7_1.py
@@ -1,96 +1,98 @@
 import RPi.GPIO as GPIO
-import time as tm
 import matplotlib.pyplot as plt
+import time
+
+# Convertation of int in arr of 8 bits
+def to_bin(n):
+    s = bin(n)[2:].zfill(8)
+    return list(map(int, s))
 
 # Get voltage at comparator
 def adc():
     k = 0
     for i in range(7, -1, -1):
-        k += 2**i
-        val1 = decimal2binary(k)
-        GPIO.output(dac, val1)
-        val2 = GPIO.input(comp)
-        tm.sleep(0.1)
-        if val2 == 1:
-            k -= 2**i
+        k += 2 ** i
+        dac_val = to_bin(k)
+        GPIO.output(dac, dac_val)
+        time.sleep(0.005)
+        cmp = GPIO.input(comp)
+        if cmp == GPIO.HIGH:
+            k -= 2 ** i
     return k
 
-# Convertation of int in arr of 8 bits
-def decimal2binary(value):
- 
-    return [int(bit) for bit in format(value, 'b').zfill(8)]
-
 # Displays val on led
-def num2_dac_leds(val):
-    sgn = decimal2binary(val)
-    GPIO.output(dac, sgn)
-    return sgn
-
-GPIO.setmode(GPIO.BCM)
+def num2_dac_leds(value):
+    signal = to_bin(value)
+    GPIO.output(dac, signal)
+    return signal
 
 dac = [8, 11, 7, 1, 0, 5, 12, 6]
 leds = [2, 3, 4, 17, 27, 22, 10, 9]
 comp = 14
 troyka = 13
-bits = 8
+bits = len(dac)
 levels = 2 ** bits
 V_max = 3.3
 
-GPIO.setup(dac, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(troyka, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(dac, GPIO.OUT)
 GPIO.setup(comp, GPIO.IN)
 
 GPIO.output(troyka, 0)
 
-mas_vlt = []
-mas_tm = []
+data_volts = []
+data_times = []
 
 try:
-    start = tm.time()
     val = 0
     GPIO.output(troyka, 1)
+    start_time = time.time()
 
-    # Charge capcitor to 210 volt
+    # Charge capcitor to 207 volt
     # Print and save voltage changes over time
-    while(val < 210):
+    while(val < 207):
         val = adc()
         print("Current volts UP: {:3}".format(val / levels * V_max), "  NUM:", val)
         num2_dac_leds(val)
-        mas_vlt.append(val / levels * V_max)
-        mas_tm.append(tm.time() - start)
-    
+        data_times.append(time.time() - start_time)
+        data_volts.append(val / levels * V_max)
+
+    discharge_start = len(data_volts)
     # Discharging capcitor
     GPIO.output(troyka, 0)
 
     # Print and save voltage changes over time
-    while(val > 64):
+    while(val > 168):
         val = adc()
         print("Current volts DOWN: {:3}".format(val / levels * V_max), "  NUM:", val)
         num2_dac_leds(val)
-        mas_vlt.append(val / levels * V_max)
-        mas_tm.append(tm.time() - start)
-    
-    end = tm.time()
+        data_times.append(time.time() - start_time)
+        data_volts.append(val / levels * V_max)
+
+    end_time = time.time()
 
     # Write parametrs in file
-    with open("./settings.txt", "w") as f:
-        f.write(str((end- start) / len(mas_vlt)))
-        f.write(("\n"))
-        f.write(str(V_max / 256))
-    
-    print(end -start, "seconds\n", len(mas_vlt) / (end - start), "\n", V_max / 256)
-        
+    with open("./settings.txt", "w") as file:
+        file.write(str((end_time - start_time) / len(data_volts)))
+        file.write(("\n"))
+        file.write(str(V_max / 256))
+
+    # Print rhe duration, period of one measurement, average sampling step and quantization step
+    print(end_time - start_time, " secs\n", len(data_volts) / (end_time - start_time), "\n", V_max / 256)
+
 finally:
     GPIO.output(dac, GPIO.LOW)
     GPIO.output(troyka, GPIO.LOW)
     GPIO.cleanup()
 
-mas_tm_str = [str(item) for item in mas_tm]
-mas_vlt_str = [str(item) for item in mas_vlt]
-
 # Write values of experiments in file
-with open("data.txt", "w") as f:
-    f.write("\n".join(mas_vlt_str))
+with open("data.txt", "w") as file:
+    for i in range(discharge_start):
+        print(f"Charging: {data_volts[i]}", file=file)
+    for i in range(discharge_start, len(data_volts)):
+        print(f"Discharging: {data_volts[i]}", file=file)
 
-plt.plot(mas_tm, mas_vlt)
+plt.plot(data_times, data_volts)
 plt.show()
